a670d2781951309498f69410bfbd71b9696e55b8
Timothee Bineau <timothee.bineau@nagra.com>
2025-11-14 12:33:48 +0100

    :wrench: update config

---
+++ flake.nix
@@ -0,0 +1,52 @@
+ description = "WSL configurations for work and home";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixos-wsl.url = "github:nix-community/NixOS-WSL";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+  };
+
+  outputs = { self, nixpkgs, home-manager, nixos-wsl, emacs-overlay }: {
+    nixosConfigurations = {
+      nagra-wsl = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.wsl
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            nixpkgs.overlays = [ emacs-overlay.overlay ];
+            networking.hostName = "nagra-wsl";
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.bineau = import ./home.nix;
+            };
+          }
+        ];
+      };
+
+      home-wsl = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          nixos-wsl.nixosModules.wsl
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            nixpkgs.overlays = [ emacs-overlay.overlay ];
+            networking.hostName = "giedi-wsl";
+            home-manager = {
+              useGlobalPkgs = true;
+              useUserPackages = true;
+              users.tim = import ./home.nix;
+            };
+          }
+        ];
+      };
+    };
+  };
+}
